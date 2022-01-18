bad8597ceea4893be8d49a964cc54ff95fde3f35
kedzierzawy2310 <kedzierzawy2310@interia.pl>
2022-01-18 12:39:39 +0100

    Update from 18/01/2022 12:39.

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -1,12 +1,54 @@
 package manager
 
 import (
+	"github.com/RIMEDO-Labs/xapp-sdk/pkg/southbound"
 	"github.com/onosproject/onos-lib-go/pkg/logging"
+	"github.com/onosproject/onos-mho/pkg/store"
 )
 
-var log = logging.GetLogger("manager")
+var log = logging.GetLogger("xApp Manager class [pkg/manager/manager.go]")
 
-func NewManager() {
+type Manager struct {
+	ID        int
+	e2Manager southbound.E2Manager
+	ueStore   store.Store
+	cellStore store.Store
+}
+
+func NewManager(ID int) *Manager {
 	// for testing only
-	log.Info("Created New Manager")
+	log.Info("Created new app manager object")
+
+	manager := &Manager{
+		ID: ID,
+	}
+
+	return manager
+}
+
+func (self *Manager) Run() {
+
+	log.Info("Manager is running")
+
+	if err := self.RunAllControllers(); err != nil {
+
+		log.Fatal("Can't run controllers [Run()].", err)
+
+	}
+
+}
+
+func (self *Manager) RunAllControllers() error {
+
+	if err := self.e2Manager.Start(); err != nil {
+
+		log.Warn("E2 Manager can't start [RunAllControllers()].", err)
+
+		return err
+
+	}
+
+	// If there's no error return nil
+	return nil
+
 }
