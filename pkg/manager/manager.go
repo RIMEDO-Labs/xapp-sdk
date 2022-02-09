3ce5257752df313feabe20e29124377770b87751
kedzierzawy2310 <kedzierzawy2310@interia.pl>
2022-02-09 13:20:08 +0100

    Switching UE between Cells function has been moved from TS xAPP to xAPP SDK.

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -3,15 +3,18 @@ package manager
 import (
 	"context"
 	"net"
+	"sync"
 
 	"github.com/RIMEDO-Labs/xapp-sdk/pkg/mho"
 	"github.com/RIMEDO-Labs/xapp-sdk/pkg/rnib"
 	"github.com/RIMEDO-Labs/xapp-sdk/pkg/southbound/e2"
 	"github.com/RIMEDO-Labs/xapp-sdk/pkg/store"
 	"github.com/RIMEDO-Labs/xapp-sdk/pkg/tspolicy"
+	e2tAPI "github.com/onosproject/onos-api/go/onos/e2t/e2"
 	e2api "github.com/onosproject/onos-api/go/onos/e2t/e2/v1beta1"
 	e2sm_mho "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_mho/v1/e2sm-mho"
 	"github.com/onosproject/onos-lib-go/pkg/logging"
+	control "github.com/onosproject/onos-mho/pkg/mho"
 	"google.golang.org/grpc"
 )
 
@@ -71,6 +74,7 @@ type Manager struct {
 	ueStore    store.Store
 	cellStore  store.Store
 	ctrlReqChs map[string]chan *e2api.ControlMessage
+	mutex      sync.RWMutex
 }
 
 func (m *Manager) Run() {
@@ -202,4 +206,63 @@ func (m *Manager) SetUe(ctx context.Context, ueData *mho.UeData) {
 
 func (m *Manager) GetControlChannelsMap(ctx context.Context) map[string]chan *e2api.ControlMessage {
 	return m.ctrlReqChs
+}
+
+func (m *Manager) SwitchUeBetweenCells(ctx context.Context, ueID string, targetCellCGI string, controlChannelsMap map[string]chan *e2api.ControlMessage) {
+
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	availableUes := m.GetUEs(ctx)
+	chosenUe := availableUes[ueID]
+
+	targetCell := m.GetCell(ctx, targetCellCGI)
+	servingCell := m.GetCell(ctx, chosenUe.CGIString)
+
+	targetCell.CumulativeHandoversOut++
+	servingCell.CumulativeHandoversIn++
+
+	m.AttachUe(ctx, &chosenUe, targetCellCGI, targetCell.CGI)
+
+	m.SetCell(ctx, targetCell)
+	m.SetCell(ctx, servingCell)
+
+	controlChannel := controlChannelsMap[chosenUe.E2NodeID]
+
+	controlHandler := &control.E2SmMhoControlHandler{
+		NodeID:            chosenUe.E2NodeID,
+		ControlAckRequest: e2tAPI.ControlAckRequest_NO_ACK,
+	}
+
+	ueIdentity := e2sm_mho.UeIdentity{
+		Value: chosenUe.UeID,
+	}
+
+	servingPlmnIDBytes := servingCell.CGI.GetNrCgi().GetPLmnIdentity().GetValue()
+	servingNCI := servingCell.CGI.GetNrCgi().GetNRcellIdentity().GetValue().GetValue()
+	servingNCILen := servingCell.CGI.GetNrCgi().GetNRcellIdentity().GetValue().GetLen()
+
+	log.Info("Creating Control Message and sending it...")
+	var err error
+	go func() {
+		if controlHandler.ControlHeader, err = controlHandler.CreateMhoControlHeader(servingNCI, servingNCILen, 1, servingPlmnIDBytes); err == nil {
+
+			if controlHandler.ControlMessage, err = controlHandler.CreateMhoControlMessage(servingCell.CGI, &ueIdentity, targetCell.CGI); err == nil {
+
+				if controlRequest, err := controlHandler.CreateMhoControlRequest(); err == nil {
+
+					controlChannel <- controlRequest
+					log.Info("Control message: ", <-controlChannel)
+
+				} else {
+					log.Warn("Control request problem :(", err)
+				}
+			} else {
+				log.Warn("Control message problem :(", err)
+			}
+		} else {
+			log.Warn("Control header problem :(", err)
+		}
+	}()
+
 }
